50c90e216a41443a91d84a0f6e5a14697828f777
brendon-lucas <brendonananda1@gmail.com>
2020-09-24 20:15:01 -0300

    Criei um mapa
    Comecei a criar um jogo.

--- src/index.js
+++ src/index.js
@@ -8,25 +8,23 @@ const config = {
   height: 600,
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("logo", logoImg);
+  
+  
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+  
+  
+}
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+function update(){
+
 }
